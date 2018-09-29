3e8341685c2a7373684c6d13ad0eb2145a121194
pekkamakkonen <pek.makkonen@gmail.com>
2018-09-29 16:52:42 +0300

    Currensies added to array

--- js/currency-converter.js
+++ js/currency-converter.js
@@ -1,3 +1,19 @@
+var currencies = [
+    [1, "Euro"],
+    [10.3208, "Ruotsin kruunu"],
+    [78.9011, "Venäjän rupla"],
+    [1.1707, "Yhdysvaltain dollari"]
+];
+console.log(currencies[0][1]);
+
+function printCurrencies() {
+
+    for (let i = 0; i < currencies.length; i++) {
+        var option = "<option value=" + (currencies[i][0]) + ">" + (currencies[i][1]) + "</option>";
+        document.write(option);
+    }
+}
+
 function printValue() {
     let amount = document.forms["currencyConverter"]["amount"].value;
     let currency1 = document.forms["currencyConverter"]["currency1"].value;
